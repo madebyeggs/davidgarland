39585496ab10fb38322d1a27e523a7aa6447f376
madebyeggs <madebyeggs@gmail.com>
2016-09-22 15:09:41 +0100

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,85 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+
+
+$(document).on('turbolinks:load', function() {
+	(function ( window, document, undefined ) {
+
+	  /*
+	   * Grab all iframes on the page or return
+	   */
+	  var iframes = document.getElementsByTagName( 'iframe' );
+
+	  /*
+	   * Loop through the iframes array
+	   */
+	  for ( var i = 0; i < iframes.length; i++ ) {
+
+	    var iframe = iframes[i],
+
+	    /*
+	       * RegExp, extend this if you need more players
+	       */
+	    players = /www.youtube.com|player.vimeo.com/;
+
+	    /*
+	     * If the RegExp pattern exists within the current iframe
+	     */
+	    if ( iframe.src.search( players ) > 0 ) {
+
+	      /*
+	       * Calculate the video ratio based on the iframe's w/h dimensions
+	       */
+	      var videoRatio        = ( iframe.height / iframe.width ) * 100;
+
+	      /*
+	       * Replace the iframe's dimensions and position
+	       * the iframe absolute, this is the trick to emulate
+	       * the video ratio
+	       */
+	      iframe.style.position = 'absolute';
+	      iframe.style.top      = '0';
+	      iframe.style.left     = '0';
+	      iframe.width          = '100%';
+	      iframe.height         = '100%';
+
+	      /*
+	       * Wrap the iframe in a new <div> which uses a
+	       * dynamically fetched padding-top property based
+	       * on the video's w/h dimensions
+	       */
+	      var wrap              = document.createElement( 'div' );
+	      wrap.className        = 'fluid-vids';
+	      wrap.style.width      = '100%';
+	      wrap.style.position   = 'relative';
+	      wrap.style.paddingTop = videoRatio + '%';
+
+	      /*
+	       * Add the iframe inside our newly created <div>
+	       */
+	      var iframeParent      = iframe.parentNode;
+	      iframeParent.insertBefore( wrap, iframe );
+	      wrap.appendChild( iframe );
+
+	    }
+
+	  }
+
+	})( window, document );
+});
