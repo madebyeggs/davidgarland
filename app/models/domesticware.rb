ba9eda750fb28ef63de036200aeb03a0b5daf0c0
madebyeggs <madebyeggs@gmail.com>
2016-09-30 13:52:29 +0100

    region added

--- app/models/domesticware.rb
+++ app/models/domesticware.rb
@@ -3,12 +3,19 @@ class Domesticware < ActiveRecord::Base
   if Rails.env.development?
       has_attached_file :image, IMAGE_PAPERCLIP_STORAGE_OPTS
     else
-      has_attached_file :image, styles: {
-          main: '1200x'
-      }
-
-      # Validate the attached image is image/jpg, image/png, etc
-      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+      has_attached_file :image,
+      :convert_options => { :all => '-quality 92' }, 
+      styles: {main: '1200x'},
+      :storage => :s3,
+      :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET'] },
+      :url => ':s3_alias_url',
+      :s3_host_alias => 'd3sk2yopf63gjr.cloudfront.net', 
+      :bucket => 'david-garland',
+      :path => "domesticwares/images/:id_partition/:style/:filename"
     end
+
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     
 end
