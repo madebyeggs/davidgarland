4c5a8a42bc26e12034c521ab3888c293a748802b
madebyeggs <madebyeggs@gmail.com>
2017-03-20 09:43:17 +0000

    work now working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/sortable
 //= require jquery-ui/effect-highlight
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ class ApplicationController < ActionController::Base
   
   def define_models
     @domesticwares = Domesticware.rank(:row_order).all
-    respond_to do |format|
-      format.html
-      format.json {render :json => @domesticwares}
-    end
     @domesticwaresdisplay = Domesticware.rank(:row_order).all
     @domesticware = Domesticware.rank(:row_order).first
     @studioceramics = Studioceramic.all
